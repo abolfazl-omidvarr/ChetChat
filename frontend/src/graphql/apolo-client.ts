d3e1e1e39ae6ddd1fc0b9e1bb38f0080c4117c09
abolfazl omidvar <abolfazl.omidvar46@gmail.com>
2023-05-13 11:16:56 +0330

    add auto refresh access token (unTested)

--- frontend/src/graphql/apolo-client.ts
+++ frontend/src/graphql/apolo-client.ts
@@ -8,11 +8,48 @@ import {
 	concat,
 } from "@apollo/client";
 
-import { getAccessToken } from "@/libs/AccessToken";
+import { getAccessToken, setAccessToken } from "@/libs/AccessToken";
+import { TokenRefreshLink } from "apollo-link-token-refresh";
+import jwtDecode from "jwt-decode";
 
 const httpLink = new HttpLink({
 	uri: "http://localhost:4000/graphql",
 	credentials: "include",
+});
+
+const refreshLink = new TokenRefreshLink({
+	accessTokenField: "accessToken",
+	isTokenValidOrUndefined: () => {
+		const token = getAccessToken();
+
+		if (!token) {
+			return true;
+		}
+
+		try {
+			const { exp } = jwtDecode<any>(token);
+
+			if (Date.now() >= exp * 1000) {
+				return false;
+			} else {
+				return true;
+			}
+		} catch (error) {
+			return false;
+		}
+	},
+	fetchAccessToken: async () => {
+		return fetch("http://localhost:4000/refresh_token", {
+			method: "POST",
+			credentials: "include",
+		});
+	},
+	handleFetch: (accessToken) => {
+		setAccessToken(accessToken);
+	},
+	handleError: (error) => {
+		console.error("Cannot refresh access token:", error);
+	},
 });
 
 const authMiddleware = new ApolloLink((operation, forward) => {
@@ -30,5 +67,6 @@ const authMiddleware = new ApolloLink((operation, forward) => {
 
 export const client = new ApolloClient({
 	cache: new InMemoryCache(),
-	link: concat(authMiddleware, httpLink),
+	// link: concat(authMiddleware, httpLink),
+	link: authMiddleware.concat(refreshLink).concat(httpLink),
 });
